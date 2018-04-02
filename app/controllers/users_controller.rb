d6b91a2314c20e2a12943334f520c71864dd9d73
Sungheek <klpoik@naver.com>
2018-04-02 01:07:07 +0000

    sign_up params update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,16 +17,19 @@ class UsersController < ApplicationController
   end
   
   def sign_up
+    reset_session
     puts "ttt"
-    url = "http://61.72.187.6/phps/join.php?nickname=#{:nickname}&nick_pass=#{:nick_pass}&id=#{params[:id]}&pwd=#{params[:pwd]}&pwd_confirmation=#{:pwd_confirmation}&phone=#{:phone}&email=#{:email}"
+    url = "http://61.72.187.6/phps/join.php?nickname=#{:nickname}&nick_pass=#{:nick_pass}&id=#{params[:id]}&pwd=#{params[:pwd]}&pwd_confirmation=#{params[:pwd_confirmation]}&phone=#{params[:phone]}&email=#{params[:email]}"
     response = HTTParty.get(url)
     hash = JSON.parse(response.body)
     
     result = hash[0]["result"]
-    error = hash[0]["error"]
+    @error = hash[0]["error"]
     
+    puts params[:phone]
     puts result 
-    puts error
+    puts @error
+    puts session[:phone]
     
     if result
       redirect_to '/home/index'
