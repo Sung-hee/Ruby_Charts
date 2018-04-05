96a5ea7afdda5fe0611f5517a30218a639e802bc
Sungheek <klpoik@naver.com>
2018-04-05 07:18:09 +0000

    title bar name update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @title = "황금알M"
     puts session[:grade].inspect
     
     if @current == false
@@ -8,58 +9,72 @@ class HomeController < ApplicationController
   end
 
   def charts
+    @title = "일반차트"
 
   end
   
   def weekcharts
+    @title = "골드차트"
     @urlparam = request.query_parameters['name']
   end
   
   def monthcharts
+    @title = "일반차트"
     @urlparam = request.query_parameters['name']
   end
 
   def tables
+    @title = "종목분석"
   end
   
   def c1charts
+    @title = "골드차트"
     @urlparam = request.query_parameters['name']
     
   end
   
   def giftcharts
+    @title = "골드차트"
     @urlparam = request.query_parameters['name']
   end
   
   def gold_daycharts
+    @title = "골드차트"
     @urlparam = request.query_parameters['name']
 
   end
   
   def gold_weekcharts
+    @title = "골드차트"
     @urlparam = request.query_parameters['name']
 
   end
 
   def gold_monthcharts
+    @title = "골드차트"
     @urlparam = request.query_parameters['name']
 
   end
   
   def foreignprice
+    @title = "주요지수"
   end
   
   def theme
+    @title = "테마주"
   end
   
   def themeStock
+    @title = "테마주"
     @urlparam = request.query_parameters['theme']
   end
   
   def pro
+    @title = "종목포착"
   end
   
   def payment
+    @title = "결제"
     
   end
 end
